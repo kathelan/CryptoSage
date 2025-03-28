42273f9d27d3ae3e0cb280a8ec1acb77138a9c8d
kathelan <mateuszdeptulski@gmail.com>
2025-03-28 23:33:08 +0100

    Added init commit for agent ai

---
+++ agent-ai/src/test/java/pl/kathelan/agentai/AgentAiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kathelan.agentai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
